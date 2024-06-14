8f2f7b3e37888c714df5840f75cc68c93d5836da
AxelMondragonHernandez <160527385+AxelMondragonHernandez@users.noreply.github.com>
2024-06-14 01:53:55 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Mondragon Hernandez Axel Uriel</p>';
+     echo '<p>Colonia:  Morelos   </p>';
+    echo '<p>Calle:  #9  </p>';
+    echo '<p>No de Casa:   32589  </p>';
+    echo '<p>Muicipio:  Coahuila   </p>';
+    echo '<p>No de telefono: 844 263 48 49 </p>';
+    ?> 
+
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,11 @@
+DE: nombre, colonia, calle, numero de casa, municipio
+DS: numero de telefono
+
+Inicio 
+Mostrar nombre completo
+Mostrar colonia   
+Mostrar calle
+Mostrar numero de casa
+Mostrar municipio  
+Mostrar numero de telefono
+Fin
